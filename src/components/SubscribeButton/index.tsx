6d9da1b67c823199e97a17c83e012d80ddff0264
Nathalia-Urenha <naty.urenha@gmail.com>
2022-01-05 23:48:10 -0300

    Componente: SubscribeButton

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.scss"
+
+export function SubscribeButton(){
+    return(
+        <button type="button" className={styles.subscribeButton} >
+            Subscribe Now
+        </button>
+    )
+}
